e4a973a36de44d442e4b936f35b532721c2aaf21
kevinelong <kevinelong@gmail.com>
2022-12-01 11:30:19 -0800

    web db api server

---
+++ webapi/server.js
@@ -0,0 +1,33 @@
+var mysql = require('mysql');
+const express = require('express');
+
+var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "S!mpl312",
+    database: "mydb"
+});
+
+function execute(sql, callback) {
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            callback(JSON.stringify(result, undefined, 4));
+        });
+    });
+}
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.header('Content-Type', 'application/json');
+    execute("SELECT id, name FROM category", data => res.send(data));
+});
+
+const port = 3000;
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
